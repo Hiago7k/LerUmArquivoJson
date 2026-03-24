4408455aab4b0f9c338cd9e0b7e984f24f04f7c2
Hiago 7k <hiago7k@gmail.com>
2026-03-24 19:05:40 -0300

    corrigindo

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿
+
+using LerUmArquivoJson;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+string path = @"C:\\Projetos\\c#\\LerUmArquivoJson\\teste.json";
+
+string arquivo = File.ReadAllText(path);
+
+Pessoas pessoas = JsonSerializer.Deserialize<Pessoas>(arquivo)!;
+
+Console.WriteLine($"{pessoas.nome}");
---
+++ Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LerUmArquivoJson;
+
+internal class Usuario
+{
+    public String? NomeDoUsuario { get;  }
+    public String? Email { get;  }
+    public int Idade { get;  }
+
+}
